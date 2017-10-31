f25ddeeba8c9eb2e1aeedda7a4a54b0cd3b085db
Basuru Balasuriya <www.basururoxyouall@gmail.com>
2017-10-31 09:18:19 +0530

    Changed main  to support Single and Multiple detectio
    Initiated SIngle Object Detection

--- Outdoor_Object_Recognition_Engine/single_detection_mode.py
+++ Outdoor_Object_Recognition_Engine/single_detection_mode.py
@@ -4,7 +4,59 @@ Author : Balasuriya B.K | IT14020254
 Single Detection Mode class is a submodule which only uses the pre-trained CNN
 to give out predictions to a single image.
 """
+import imports as IMPORT_MANAGER
+import cv2
 
 class SingleDetection:
 
-    
+    IMPORT_MANAGER = None
+    Image = None
+    Prediction = None
+    Frame = None
+    CameraController = None
+    CameraID = None
+
+    def __init__(self, import_manager, camera_id):
+        self.CameraID = camera_id
+        self.IMPORT_MANAGER = import_manager
+        self.CameraController = cv2.VideoCapture(self.CameraID)
+
+        # Set the Resolution of the Camera to 1024 x 768
+        self.CameraController.set(cv2.CAP_PROP_FRAME_HEIGHT, 1400)
+        self.CameraController.set(cv2.CAP_PROP_FRAME_WIDTH, 1150)
+
+    def track_object(self):
+
+
+
+    def detect(self, unprocessed_image):
+        self.Image = self.preprocessed_image(unprocessed_image)
+        self.Prediction = self.predict_for_single_image(self.Image)
+
+        return self.Prediction
+
+    @staticmethod
+    def get_labels_for_classes():
+        # return ['car', 'cat', 'dog', 'shoe']
+        return ['car', 'cat', 'dog']
+
+    # Run prediction for a single image
+    def predict_for_single_image(self, image):
+        # label the images according the folder structure
+        labels = self.get_labels_for_classes()
+        out = self.classifier.predict(image, verbose=0)[0]
+
+        return out
+
+    # Pre-process the image as for classifier requirements
+    def preprocessed_image(self, image):
+        processed_image = self.IMPORT_MANAGER.Image.fromarray(image)
+        processed_image = processed_image.resize((64, 64))
+        processed_image = self.IMPORT_MANAGER.keras_preprocess.img_to_array(processed_image)
+        processed_image = self.IMPORT_MANAGER.np.expand_dims(processed_image, axis=0)
+
+        return processed_image
+
+
+
+ftry = SingleDetection(IMPORT_MANAGER)
--- main.py
+++ main.py
@@ -9,6 +9,7 @@ TODO: 1. Change the Outdoor object recognition module to recognize one object at
 """
 
 import imports as IMPORT_MANAGER
+import config
 from Outdoor_Object_Recognition_Engine.grid_based_probability_detection import GBPD
 from Outdoor_Object_Recognition_Engine.hand_movement_tracking_module import TrackHand
 from Outdoor_Object_Recognition_Engine.point_to_grid_mapper import PointToFingerMapper
@@ -19,60 +20,66 @@ from Dialogue_Manager.text_to_speech_processesor import TextToSpeech
 # Initiate Dialogue Manager
 Text_To_Speech = TextToSpeech(IMPORT_MANAGER)
 
-# Initiate Outdoor Object Recognition Module & Hand Tracking Module
-Hand_Tracker = TrackHand(threshold=70, camera=0, blur_value=21)
-Grid_Based_Probability_Detection = GBPD(imports=IMPORT_MANAGER, classifier=IMPORT_MANAGER.outdoor_objects_classifier,
-                                        window_size=(256, 256))
-# Track the hand
-captured_frame, finger_location = Hand_Tracker.track_hand()
-print("Finger Location", finger_location)
+# Check if its Multiple Object Detection Platform
+if config.Configurations.PLATFORM_MODE == 2:
+    # Initiate Outdoor Object Recognition Module & Hand Tracking Module
+    Hand_Tracker = TrackHand(threshold=70, camera=0, blur_value=21)
+    Grid_Based_Probability_Detection = GBPD(imports=IMPORT_MANAGER, classifier=IMPORT_MANAGER.outdoor_objects_classifier,
+                                            window_size=(256, 256))
+    # Track the hand
+    captured_frame, finger_location = Hand_Tracker.track_hand()
+    print("Finger Location", finger_location)
 
-# Calculate the time for GBPD execution time
-start_time = IMPORT_MANAGER.time.time()
-# image_stream = IMPORT_MANAGER.Image.open('Outdoor_Object_Recognition_Engine/custom_test/dog.6.jpg')
-captured_frame = IMPORT_MANAGER.Image.fromarray(captured_frame, mode="RGB")
-image_coordinates_with_predictions = Grid_Based_Probability_Detection.main(captured_frame)
-print("GBPD algorithm Execution Time: ", IMPORT_MANAGER.time.time() - start_time)
+    # Calculate the time for GBPD execution time
+    start_time = IMPORT_MANAGER.time.time()
+    # image_stream = IMPORT_MANAGER.Image.open('Outdoor_Object_Recognition_Engine/custom_test/dog.6.jpg')
+    captured_frame = IMPORT_MANAGER.Image.fromarray(captured_frame, mode="RGB")
+    image_coordinates_with_predictions = Grid_Based_Probability_Detection.main(captured_frame)
+    print("GBPD algorithm Execution Time: ", IMPORT_MANAGER.time.time() - start_time)
 
-fig, ax = IMPORT_MANAGER.plt.subplots(1)
-ax.imshow(captured_frame)
+    fig, ax = IMPORT_MANAGER.plt.subplots(1)
+    ax.imshow(captured_frame)
 
-# Extract the Regions // Ignore the index 0
-for prediction, image_coordinates in image_coordinates_with_predictions[1:]:
-    print(prediction, image_coordinates)
-    x, y, w, h = image_coordinates
-    color = IMPORT_MANAGER.randomize_color()
-    rect = IMPORT_MANAGER.patches.Rectangle((x, y), w, h, linewidth=3, edgecolor=color, facecolor='none', )
-    IMPORT_MANAGER.plt.text(x, y, prediction, color=color)
-    ax.add_patch(rect)
+    # Extract the Regions // Ignore the index 0
+    for prediction, image_coordinates in image_coordinates_with_predictions[1:]:
+        print(prediction, image_coordinates)
+        x, y, w, h = image_coordinates
+        color = IMPORT_MANAGER.randomize_color()
+        rect = IMPORT_MANAGER.patches.Rectangle((x, y), w, h, linewidth=3, edgecolor=color, facecolor='none', )
+        IMPORT_MANAGER.plt.text(x, y, prediction, color=color)
+        ax.add_patch(rect)
 
-IMPORT_MANAGER.plt.show()
+    IMPORT_MANAGER.plt.show()
 
-# Initiate Finger to GBPD Mapper
-Pointer_To_Location = PointToFingerMapper(image_coordinates_with_predictions[1:], finger_location)
-prediction_and_selected_box = Pointer_To_Location.main()
+    # Initiate Finger to GBPD Mapper
+    Pointer_To_Location = PointToFingerMapper(image_coordinates_with_predictions[1:], finger_location)
+    prediction_and_selected_box = Pointer_To_Location.main()
 
-print("Predicted : {}".format(prediction_and_selected_box))
-if len(prediction_and_selected_box) is 0:
-    print("The Object user is pointing cannot be identified")
-    Text_To_Speech.speak("The Object user is pointing cannot be identified")
+    print("Predicted : {}".format(prediction_and_selected_box))
+    if len(prediction_and_selected_box) is 0:
+        print("The Object user is pointing cannot be identified")
+        Text_To_Speech.speak("The Object user is pointing cannot be identified")
 
-read_image = IMPORT_MANAGER.imutils.imread("Outdoor_Object_Recognition_Engine/edited.jpg")
+    read_image = IMPORT_MANAGER.imutils.imread("Outdoor_Object_Recognition_Engine/edited.jpg")
 
-fig, ax = IMPORT_MANAGER.plt.subplots(1)
-ax.imshow(read_image)
+    fig, ax = IMPORT_MANAGER.plt.subplots(1)
+    ax.imshow(read_image)
 
-for prediction, image_coordinates in prediction_and_selected_box:
-    print(prediction, image_coordinates)
-    string = "You pointed at a "
-    Text_To_Speech.predict_speech(sentence=string, prediction=prediction)
-    x, y, w, h = image_coordinates
-    color = IMPORT_MANAGER.randomize_color()
-    rect = IMPORT_MANAGER.patches.Rectangle((x, y), w, h, linewidth=3, edgecolor=color, facecolor=color, alpha=0.5)
-    IMPORT_MANAGER.plt.text(x, y, prediction, color=color)
-    ax.add_patch(rect)
+    for prediction, image_coordinates in prediction_and_selected_box:
+        print(prediction, image_coordinates)
+        string = "You pointed at a "
+        Text_To_Speech.predict_speech(sentence=string, prediction=prediction)
+        x, y, w, h = image_coordinates
+        color = IMPORT_MANAGER.randomize_color()
+        rect = IMPORT_MANAGER.patches.Rectangle((x, y), w, h, linewidth=3, edgecolor=color, facecolor=color, alpha=0.5)
+        IMPORT_MANAGER.plt.text(x, y, prediction, color=color)
+        ax.add_patch(rect)
 
-IMPORT_MANAGER.plt.show()
+    IMPORT_MANAGER.plt.show()
+
+# Check if its Single Object Detection Platform
+elif config.Configurations.PLATFORM_MODE == 1:
+    print("Single Detection")
 
 # Initiate Description Generator Module
 
