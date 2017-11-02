711a90437e827347c06f050b5fedc2d072439de3
Basuru Balasuriya <www.basururoxyouall@gmail.com>
2017-11-02 13:47:12 +0530

    Added Singleton Pattern for Configurations
    Fixed a bug where multiple instances carried multiple values for configurations in different threads

--- config.py
+++ config.py
@@ -5,62 +5,117 @@ Unauthorized edits are not allowed.
 This file is acting as the settings file for the program which corresponds to Outdoor and Indoor Object Recognition
 Engine.
 """
+import threading
 
 
-class Configurations:
+class Configurations(object):
+
+    _instance = None
+    _lock = threading.Lock()
+
+    # Use a Singleton Object to initiate Thread safe Configurations Instance
+    def __new__(cls, *args, **kwargs):
+        """
+        :return: Single object of Configuration class
+        """
+        with Configurations._lock:
+            if Configurations._instance is None:
+                Configurations._instance = super(Configurations, cls).__new__(cls)
+        return Configurations._instance
 
     """
     ENVIRONMENT MODE:-
         1 => Indoor Object Detection (default)
         2 => Outdoor Object Detection
     """
-    ENVIRONMENT_MODE = 1
+    ENVIRONMENT_MODE = 2
 
     def get_environment_mode(self):
+        """
+        :return: current ENVIRONMENT_MODE
+        """
         return self.ENVIRONMENT_MODE
 
     def set_environment_mode(self):
+        """
+        Changes the environment to next one
+        :return: next ENVIRONMENT_MODE
+        """
         if self.is_indoor_mode():
             self.set_environment_mode_outdoor()
         elif self.is_outdoor_mode():
             self.set_environment_mode_indoor()
 
     def set_environment_mode_indoor(self):
+        """
+        Set mode to Indoor Detection
+        """
         self.ENVIRONMENT_MODE = 1
 
     def set_environment_mode_outdoor(self):
+        """
+        Set mode to Outdoor Detection
+        """
         self.ENVIRONMENT_MODE = 2
 
     def is_indoor_mode(self):
+        """
+        :return: True if Indoor Mode
+        """
         return self.ENVIRONMENT_MODE == 1
 
     def is_outdoor_mode(self):
+        """
+        :return: True if Outdoor Mode
+        """
         return self.ENVIRONMENT_MODE == 2
 
     """
      PLATFORM_MODE:-
         1 => Single Object Detection Mode (default)
         2 => Multiple Object Detection Mode
+        
+    Used in Outdoor Object detection only
     """
     PLATFORM_MODE = 1
 
     def get_platform_mode(self):
+        """
+        :return: current PLATFORM_MODE
+        """
         return self.PLATFORM_MODE
 
     def set_platform_mode(self, plt_mode):
+        """
+        Set the platform mode to the given mode
+        :param plt_mode: platform mode [single/multiple]
+        :return:
+        """
         self.PLATFORM_MODE = plt_mode
 
     def set_platform_mode_single_detection(self):
+        """
+        Set Platform for Single detection
+        """
         self.PLATFORM_MODE = 1
 
     def set_platform_mode_multiple_detection(self):
+        """
+        Set Platform for Multiple detection
+        """
         self.PLATFORM_MODE = 2
 
     def is_single_object_detection_mode(self):
+        """
+        :return: True if Single Detection Mode
+        """
         return self.PLATFORM_MODE == 1
 
     def is_multiple_object_detection_mode(self):
-        return self.PLATFORM_MODE == 0
+        """
+        :return: True if Multiple Detection Mode
+        """
+        return self.PLATFORM_MODE == 2
 
     """
     HAND_TRACKER:-
